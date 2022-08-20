ea6b25ecae2378c0107ff556bcc8980a046be95f
Hong-HH <a1guskn7@gmail.com>
2022-08-21 00:25:03 +0900

    photo_update_complete

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from resources.login import UserLoginResource
 from resources.logout import LogoutResource
 from resources.blocklist import check_blocklist
 from resources.photo import PhotoResource
+from resources.photo_change import PhotoChangeResource
 
 
 app = Flask(__name__)
@@ -39,6 +40,7 @@ api.add_resource(UserRegisterResource, '/v1/user/register')     # 회원가입
 api.add_resource(UserLoginResource, '/v1/user/login')           # 로그인
 api.add_resource(LogoutResource, '/v1/user/logout')             # 로그아웃
 api.add_resource(PhotoResource, '/v1/photo')                    # 사진 업로드 / 조회
+api.add_resource(PhotoChangeResource, '/v1/photo/<int:photo_id>')# 사진의 설명 변경, 사진 삭제
 
 
 
