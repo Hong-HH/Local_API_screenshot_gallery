5ab8273cb327e73ca47b09ffa4cb3a9cf1ffcb5c
Hong-HH <a1guskn7@gmail.com>
2022-08-23 10:11:07 +0900

    tag_delete_added

--- resources/rekognition.py
+++ resources/rekognition.py
@@ -174,7 +174,6 @@ class GetTagResource(Resource) :
 
     @jwt_required() 
     def post(self, photo_id) :
-        user_id = get_jwt_identity()
         data = request.get_json()
         tag_list = data["tag"]
 
@@ -191,6 +190,46 @@ class GetTagResource(Resource) :
                             (%s, %s);'''
                 # 파이썬에서, 튜플만들때, 데이터가 1개인 경우에는 콤마를 꼭 써주자.
                 record = (photo_id, new_tag)
+                # 3. 커넥션으로부터 커서를 가져온다.
+                cursor = connection.cursor()
+
+                # 4. 쿼리문을 커서에 넣어서 실행한다. // 실제로 실행하는 것은 커서가 해준다.
+                # 레코드는 직접입력말고 변수로 넣었을때 실행
+                cursor.execute(query, record)
+
+                # 5. 커넥션을 커밋한다. => 디비에 영구적으로 반영하라는 뜻.
+                connection.commit()
+
+
+        except Error as e:
+            print('Error', e)
+            return {'error' : 400, 'result' : str(e)}, HTTPStatus.BAD_REQUEST
+        # finally는 필수는 아니다.
+        finally :
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print('MySQL connection is closed')
+                return {'error' : 200, 'result' : '잘 저장되었습니다.'}, HTTPStatus.OK
+
+
+
+    @jwt_required() 
+    def delete(self, photo_id) :
+        data = request.get_json()
+        tag_list = data["tag"]
+
+        try : 
+            # 1. DB에 연결
+            connection = get_connection()
+
+            for old_tag in tag_list :
+
+                # 2. 쿼리문 만들기 : mysql workbench 에서 잘 되는것을 확인한 SQL문을 넣어준다.
+                query = '''delete from tag
+                    where photo_id = %s and tag = %s;'''
+                # 파이썬에서, 튜플만들때, 데이터가 1개인 경우에는 콤마를 꼭 써주자.
+                record = (photo_id, old_tag)
                 # 3. 커넥션으로부터 커서를 가져온다.
                 cursor = connection.cursor()
 
